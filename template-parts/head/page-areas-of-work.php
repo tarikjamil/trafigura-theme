bc7a56a7a898652842c23e45ed447cf22ddc03d1
tarikjamil <tarikjamill@gmail.com>
2026-08-13 11:09:39 +0100

    update to fixe CLS

--- template-parts/head/page-areas-of-work.php
+++ template-parts/head/page-areas-of-work.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta content="width=device-width, initial-scale=1" name="viewport">
-<?php wp_enqueue_style('trafigura-bundle', get_template_directory_uri() . '/assets/css/trafigura-bundle.css', [], '1786552000'); ?>
+<?php wp_enqueue_style('trafigura-bundle', get_template_directory_uri() . '/assets/css/trafigura-bundle.css', [], '1786553000'); ?>
 <?php get_template_part('template-parts/head/partials/bebas-font'); ?>
-<link href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png?v=1786552000" rel="shortcut icon" type="image/x-icon">
-<link href="<?php echo get_template_directory_uri(); ?>/assets/images/webclip.png?v=1786552000" rel="apple-touch-icon"><?php get_template_part('template-parts/head/partials/gtm-deferred'); ?>
+<link href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png?v=1786553000" rel="shortcut icon" type="image/x-icon">
+<link href="<?php echo get_template_directory_uri(); ?>/assets/images/webclip.png?v=1786553000" rel="apple-touch-icon"><?php get_template_part('template-parts/head/partials/gtm-deferred'); ?>
